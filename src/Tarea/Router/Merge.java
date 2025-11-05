57f695a38b2b1a7ea581ba5483f80e567914b509
Alejandro <alejandro.minchon@alu.uhu.es>
2025-11-05 09:45:44 +0100

    merge

--- src/Tarea/Router/Merge.java
+++ src/Tarea/Router/Merge.java
@@ -1,5 +1,23 @@
 package Tarea.Router;
 
-public class Merge {
+import Principal.Mensaje;
+import Principal.Slot;
+import Tarea.TareaBase;
+import java.util.List;
 
+public class Merge extends TareaBase {
+     public Merge(List<Slot> entradas, Slot salida) {
+        super(entradas, List.of(salida));
+    }
+
+    @Override
+    public void execute() {
+        for (Slot entrada : entradas){
+            if(!entrada.isEmptyQueue()){
+                Mensaje mensaje = entrada.dequeuePoll();
+                salidas.getFirst().enqueue(mensaje);
+                break;
+            }
+        }
+    }
 }
