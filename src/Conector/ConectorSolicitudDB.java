2817efa6535e13ae9574cdf458cdd73f6fa950e7
Alejandro <alejandro.minchon@alu.uhu.es>
2025-11-12 12:49:19 +0100

    ConectorSolicitudDB

--- src/Conector/ConectorSolicitudDB.java
+++ src/Conector/ConectorSolicitudDB.java
@@ -3,7 +3,13 @@ package Conector;
 import Puerto.Puerto;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 public class ConectorSolicitudDB extends Conector {
 
@@ -22,6 +28,52 @@ public class ConectorSolicitudDB extends Conector {
     }
 
     public void execute() {
+        // Obtiene el documento XML de entrada desde el slot de entrada del puerto
+        Document docEntrada = puerto.getDocumentBySlot();
         
+        if (docEntrada == null) {
+            // No hay nada que procesar
+            return;
+        }
+
+        // Extrae la consulta SQL del XML (asumiendo <sql>...</sql>)
+        String query = docEntrada.getElementsByTagName("sql").item(0).getTextContent();
+        System.out.println("ConectorSQLAzure: Ejecutando query: " + query);
+
+        try (Statement stmt = con.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
+
+            // Crea un nuevo documento XML para la respuesta
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document newDoc = builder.newDocument();
+
+            // Construye el XML de respuesta a partir del ResultSet
+            Element raiz = newDoc.createElement("resultadoSQL");
+            newDoc.appendChild(raiz);
+
+            while (rs.next()) {
+                Element fila = newDoc.createElement("fila");
+                raiz.appendChild(fila);
+
+                int numColumnas = rs.getMetaData().getColumnCount();
+                for (int i = 1; i <= numColumnas; i++) {
+                    String nombreColumna = rs.getMetaData().getColumnName(i);
+                    String valorColumna = rs.getString(i);
+
+                    Element elementoCol = newDoc.createElement(nombreColumna);
+                    elementoCol.setTextContent(valorColumna);
+                    fila.appendChild(elementoCol);
+                }
+            }
+
+            // Pone el documento de respuesta en el puerto
+       
+            puerto.setDocument(newDoc);
+
+        } catch (Exception e) {
+            System.out.println("ConectorSQLAzure: Error al ejecutar la consulta: " + e.getMessage());
+
+        }
     }
 }
